806748a1ba16f741b0d414f7765d04b9923a3407
cafedeewane <cafedeewane@gmail.com>
2021-11-10 23:32:25 +0530

    Adding Website

---
+++ js/index.js
@@ -0,0 +1,418 @@
+gsap.registerPlugin(ScrollTrigger);
+
+
+gsap.fromTo(".item-a", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-a",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-b", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-b",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-c", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-c",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-d", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-d",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.3);
+
+gsap.fromTo(".item-aa", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-aa",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-ab", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ab",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-ac", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ac",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-ba", {
+    opacity: 0,
+    x: 300,
+    duration: 40,
+}, {
+    scrollTrigger: {
+        trigger: ".item-ba",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-bb", {
+    opacity: 0,
+    x: 300,
+    duration: 40,
+}, {
+    scrollTrigger: {
+        trigger: ".item-bb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-bc", {
+    opacity: 0,
+    x: 300,
+    duration: 40,
+}, {
+    scrollTrigger: {
+        trigger: ".item-bc",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+
+gsap.fromTo(".item-ca", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ca",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-cb", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-cb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-cc", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-cc",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-da", {
+
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-da",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-db", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-db",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-ea", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ea",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-eb", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-eb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-fa", {
+
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-fa",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-fb", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-fb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-fc", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-fc",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-ga", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ga",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-gb", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-gb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+
+gsap.fromTo(".item-ha", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ha",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-hb", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-hb",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-hc", {
+    opacity: 0,
+    x: 300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-hc",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-ia", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ia",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+});
+
+gsap.fromTo(".item-ib", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ib",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.1);
+
+gsap.fromTo(".item-ic", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ic",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.2);
+
+gsap.fromTo(".item-id", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-id",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.3);
+
+gsap.fromTo(".item-ie", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ie",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.4);
+
+gsap.fromTo(".item-if", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-if",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.5);
+
+gsap.fromTo(".item-ig", {
+
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ig",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.6);
+
+gsap.fromTo(".item-ih", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ih",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.7);
+
+gsap.fromTo(".item-ij", {
+    opacity: 0,
+    x: -300,
+    duration: 20,
+
+}, {
+    scrollTrigger: {
+        trigger: ".item-ij",
+        toggleActions: "restart pause restart none"
+    }, opacity: 1, x: 0
+}).delay(0.8);
